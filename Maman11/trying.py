5a0ac145f8652639eb9eb7e6711e3b301ea1a173
inbar360 <inbarot21@gmail.com>
2024-10-20 00:55:08 +0300

    add python directories to gitignore

--- Maman11/trying.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print(open('moshe', 'rb').read())
-print(open('moshe', 'r').read().encode('utf-8'))
-path = os.fil
--- Maman13/Q1.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-
-    b_list = [f"B{word[1:].lower()}" for word in sys.argv if word[0] == 'b']
-    line = input("Enter the sentence: ")
-    words = [word.upper() for word in line.split() if 'o' in word]
-    print(', '.join(words))
